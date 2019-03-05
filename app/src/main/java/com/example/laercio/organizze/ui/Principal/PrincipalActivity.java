7281cd9ad7d331893eb602d035555418d80a4fa4
Laercio Diniz <laerciodiniz@gmail.com>
2019-03-05 13:52:38 -0300

    App finalizado.

--- app/src/main/java/com/example/laercio/organizze/ui/Principal/PrincipalActivity.java
+++ app/src/main/java/com/example/laercio/organizze/ui/Principal/PrincipalActivity.java
@@ -1,20 +1,25 @@
 package com.example.laercio.organizze.ui.Principal;
 
+import android.content.ClipData;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.card.MaterialCardView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.laercio.organizze.R;
 import com.example.laercio.organizze.config.ConfiguracaoFirebase;
@@ -62,7 +67,8 @@ public class PrincipalActivity extends AppCompatActivity {
     private ValueEventListener valueEventListenerMovimentacoes;
 
     private AdapterMovimentacao adapterMovimentacao;
-    private List<Movimentacao> movimentacaos = new ArrayList<>();
+    private List<Movimentacao> movimentacoes = new ArrayList<>();
+    private Movimentacao movimentacao;
     private String mesAnoSelecionado;
 
     private Double despesaTotal = 0.0;
@@ -79,9 +85,10 @@ public class PrincipalActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         ConfiguraCalendarView();
+        swipe();
 
         //Configura Adapter
-        adapterMovimentacao = new AdapterMovimentacao(movimentacaos,this);
+        adapterMovimentacao = new AdapterMovimentacao(movimentacoes,this);
 
         //Configura RecyclerView
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
@@ -91,23 +98,92 @@ public class PrincipalActivity extends AppCompatActivity {
 
 
     }
+    //Deslizar linhas do recyclerview
+    public void swipe(){
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_principal, menu);
-        return super.onCreateOptionsMenu(menu);
+        ItemTouchHelper.Callback itemTouch = new ItemTouchHelper.Callback() {
+            @Override
+            public int getMovementFlags(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder) {
+                //Desabilita drag and drop
+                int dragFlags = ItemTouchHelper.ACTION_STATE_IDLE;
+                //Arrasta para direita e para esquerda
+                int swipeFlags = ItemTouchHelper.START | ItemTouchHelper.END;
+                return makeMovementFlags( dragFlags, swipeFlags );
+
+            }
+
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder viewHolder1) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+                excluirMovimentacao( viewHolder );
+            }
+        };
+        //Anexar configuração do swipe no recyclerview
+        new ItemTouchHelper( itemTouch ).attachToRecyclerView( recyclerView );
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.menuSair :
-                autenticacao.signOut();
-                startActivity(new Intent(this, MainActivity.class));
-                finish();
-                break;
+    public void excluirMovimentacao(final RecyclerView.ViewHolder viewHolder){
+
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        //Configura AlertDialog
+        alertDialog.setTitle("Excluir movimentação da conta");
+        alertDialog.setMessage("Você tem certeza que deseja realmente excluir essa movimentacao da sua conta?");
+        alertDialog.setCancelable(false);
+
+        alertDialog.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                int position = viewHolder.getAdapterPosition();
+                movimentacao = movimentacoes.get( position );
+
+                String emailUsuario = autenticacao.getCurrentUser().getEmail();
+                String idUsuario = Base64Custom.codificarBase64( emailUsuario );
+
+                movimentacaoRef = firebaseRef.child("movimentacao")
+                        .child( idUsuario )
+                        .child( mesAnoSelecionado );
+
+                movimentacaoRef.child( movimentacao.getKey() ).removeValue();
+                adapterMovimentacao.notifyItemRemoved( position );
+                atualizarSaldo();
+
+            }
+        });
+
+        alertDialog.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(PrincipalActivity.this,"Cancelado", Toast.LENGTH_LONG).show();
+                adapterMovimentacao.notifyDataSetChanged();
+            }
+        });
+
+        AlertDialog alert = alertDialog.create();
+        alert.show();
+
+    }
+
+    public void atualizarSaldo(){
+
+        String emailUsuario = autenticacao.getCurrentUser().getEmail();
+        String idUsuario = Base64Custom.codificarBase64( emailUsuario );
+        usuarioRef = firebaseRef.child("usuarios").child( idUsuario );
+
+        if ( movimentacao.getTipo().equals("r") ) {
+            receitaTotal = receitaTotal - movimentacao.getValor();
+            usuarioRef.child("receitaTotal").setValue(receitaTotal);
         }
-        return super.onOptionsItemSelected(item);
+
+        if ( movimentacao.getTipo().equals("d") ) {
+            despesaTotal = despesaTotal - movimentacao.getValor();
+            usuarioRef.child("despesaTotal").setValue(despesaTotal);
+        }
+
     }
 
     public void recuperarMovimentacoes(){
@@ -123,12 +199,13 @@ public class PrincipalActivity extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-                movimentacaos.clear();
+                movimentacoes.clear();
 
                 for (DataSnapshot dados: dataSnapshot.getChildren() ){
 
                     Movimentacao movimentacao = dados.getValue( Movimentacao.class );
-                    movimentacaos.add( movimentacao );
+                    movimentacao.setKey( dados.getKey() );
+                    movimentacoes.add( movimentacao );
 
                 }
 
@@ -207,6 +284,25 @@ public class PrincipalActivity extends AppCompatActivity {
         });
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menuSair :
+                autenticacao.signOut();
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
     @Override
     protected void onStart() {
